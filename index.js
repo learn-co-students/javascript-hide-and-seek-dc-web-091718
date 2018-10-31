303f4acd2b5e28c0d9e7b18cc3ff78ff0548a8eb
pasolre95kim <pasolre95kim@gmail.com>
2018-10-30 21:41:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target")
+}
+
+function deepestChild() {
+  let selector = document.getElementById("grand-node").querySelectorAll("div")
+    return selector[selector.length -1]
+}
+
+// function increasedRankBy(n) {
+//   let ranked = document.querySelectorAll('.ranked-list')
+//   for (let i = 0, l = children.length; i < l; i++) {
+//
+//   }
+// }
