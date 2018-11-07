490cf2ea896c0ce607f1d1725da1a919464eaf68
suttonau <sutton_mjbc@icloud.com>
2018-11-06 23:06:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const getFirstSelector = function(selector){
+  return document.querySelector(selector);
+}
 
+const nestedTarget = function(){
+  return document.querySelector('#nested .target');
+}
+
+const increaseRankBy = function(n) {
+  const rankedList = document.querySelectorAll('.ranked-list li')
+
+  return Array.from(rankedList).forEach(function(listEl){
+    listEl.innerHTML = parseInt(listEl.innerHTML) + parseInt(n);
+    return listEl.innerHTML;
+  })
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
